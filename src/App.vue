1b0a82b862e73099762473d36c5f6e9ba4d53d05
Caleb Gibbs <calebgibbs@me.com>
2021-04-25 23:39:44 +1200

    navigation

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div> 
+    <TheMenu/>
+  </div>
+</template>
+
+<script>
+  import TheMenu from './components/TheMenu'
+  export default {
+    name: 'App', 
+    components: {
+      TheMenu
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TheMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="nav">
+    <div class="logo">CG</div> 
+    <div class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/projects">Projects</router-link> 
+      <router-link to="/playground">Playground</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'menu'
+  }
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex; 
+  background: rgba(0,0,0,0.8);   
+  backdrop-filter: saturate(180%) blur(20px);
+  font-size: 1.1rem; 
+  padding: 0.6rem; 
+  align-items: center; 
+  justify-content: space-between;
+}  
+
+.logo {
+  font-weight: bold; 
+  color: #fff; 
+}  
+
+$link-padding: 2rem;
+
+a { 
+  color:#f5f5f7; 
+  text-decoration: none; 
+  padding: 0 $link-padding 0 $link-padding;   
+  opacity: .8; 
+  transition: opacity ease-in-out 0.1s;
+  &:hover { 
+    opacity: 1;
+  }
+} 
+</style>
