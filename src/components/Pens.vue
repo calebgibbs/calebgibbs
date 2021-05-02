6a1c1a3017e6e262defd69acc115117b08dc1ab5
Caleb Gibbs <calebgibbs@me.com>
2021-05-02 17:13:20 +1200

    pens from json fetch data

--- src/components/Pens.vue
+++ src/components/Pens.vue
@@ -1,22 +1,28 @@
 <template>
-  <div>
-    <VueRssFeed :feedUrl="feedUrl" :name="name" :limit="limit"/>
+  <div class="pens-grid"> 
+    <div v-for="pen in pens.items" :key="pen.url">
+      <a :href="pen.link" target="_blank">
+        <div class="pen">
+          <img :src="pen.thumbnail" alt="Codepen thumbnail" style="width: 25%">
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
 <script> 
-  import VueRssFeed from 'vue-rss-feed'
   export default {
-    name: 'Code pens', 
-    components: {
-      VueRssFeed
-    }, 
+    name: 'Codepens', 
     data() { 
       return {
-        feedUrl: 'https://codepen.io/calebgibbs/public/feed',
-        name: 'Pens',
-        limit: 5,
+        pens: []
       }
+    },
+    mounted() {
+      fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fcodepen.io%2Fcalebgibbs%2Fpublic%2Ffeed')
+      .then(res => res.json()) 
+      .then(data => this.pens = data) 
+      .catch(err => console.log(err.message))
     }
 
 
