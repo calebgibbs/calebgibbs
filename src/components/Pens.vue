b5812b50c38e6d0310b5431900cf256784996d1f
Caleb Gibbs <calebgibbs@me.com>
2021-05-02 23:31:23 +1200

    hover transitions

--- src/components/Pens.vue
+++ src/components/Pens.vue
@@ -10,7 +10,7 @@
             <span class="title">{{pen.title}}</span>  
           </div> 
           <div class="date">
-            <span><small>{{ pen.pubDate | limit-10 }}</small></span>
+            <span class="date-text"><small>{{ pen.pubDate | limit-10 }}</small></span>
             <img class="external-link" src="../assets/images/icons/external-link.svg" alt="external link">
           </div>
         </div>
@@ -47,9 +47,9 @@ $cardTransition: 0.2s;
   display: grid; 
   grid-gap: 2em;
   width: 90%; 
-  margin: 0 auto; 
-  margin-top: 4em; 
-  
+  margin: 0 auto;  
+  margin-top: 4em;
+
   @include desktop-large {
     grid-template-columns: repeat(4, 1fr);
   } 
@@ -68,7 +68,7 @@ $cardTransition: 0.2s;
   @include mobile-portait {
     grid-template-columns: repeat(1, 1fr);
   } 
-}   
+}    
 
 .pen-img-container {
   overflow: hidden; 
@@ -85,36 +85,45 @@ a {
   color: #000; 
   padding: .5em; 
   background: #f9f9f9;  
-  transition: transform $cardTransition;   
+  transition: transform 0.3s, box-shadow 0.4s;  
   font-size: 1em; 
-  overflow: hidden;
+  overflow: hidden; 
 
   &-text {
     display: flex; 
-    justify-content: space-between;
+    justify-content: space-between; 
+    margin: 0.5em 0 0.5em 0.2em;
   }
 
   &:hover {
-    transform: scale(1.03);
+    transform: scale(1.03);  
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.3);
   }  
 
   &:hover .pen-img {
     transform: scale(1.05); 
   }
   
-  &:hover .date {
+  &:hover .external-link{
     opacity: 1; 
   }
 }  
 
-.date { 
-  opacity: 0; 
+.date {  
   transition: opacity $cardTransition; 
   display: flex; 
   justify-content: space-between;  
-  color: #606060;
+  align-items: center; 
+  color: #606060; 
+
   img { 
-    width: 1em;
-  }
+    width: 1.3em;  
+    opacity: 0; 
+    transition: opacity 0.3s;
+  } 
+} 
+
+.date-text {
+  font-size: .7em;
 }
 </style>
