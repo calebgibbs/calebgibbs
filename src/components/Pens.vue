94572c572e3154182927d9c225b161b22c521491
Caleb Gibbs <calebgibbs@me.com>
2021-05-08 14:38:57 +1200

    favicon

--- src/components/Pens.vue
+++ src/components/Pens.vue
@@ -6,12 +6,14 @@
           <div class="pen-img-container">
             <img :src="pen.thumbnail" alt="thumbnail" style="width: 100%" class="pen-img"> 
           </div>
-          <div class="pen-text">
-            <span class="title">{{pen.title}}</span>  
-          </div> 
-          <div class="date">
-            <span class="date-text"><small>{{ pen.pubDate | limit-10 }}</small></span>
-            <img class="external-link" src="../assets/images/icons/external-link.svg" alt="external link">
+          <div class="pen-text-outer">
+            <div class="pen-title">
+              <span class="title">{{pen.title}}</span>  
+            </div> 
+            <div class="pen-date">
+              <span class="date-text"><small>{{ pen.pubDate | limit-10 }}</small></span>
+              <img class="external-link" src="../assets/images/icons/external-link.svg" alt="external link">
+            </div>
           </div>
         </div>
       </a>
@@ -51,16 +53,16 @@ $cardTransition: 0.2s;
   margin-top: 4em;
 
   @include desktop-large {
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
   } 
   @include desktop-small {
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
   } 
   @include tablet-landscape {
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
   } 
   @include tablet-portait {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
   } 
   @include mobile-landscape {
     grid-template-columns: repeat(2, 1fr);
@@ -83,45 +85,40 @@ a {
 
 .pen { 
   color: #000; 
-  padding: .5em; 
   background: #f9f9f9;  
   transition: transform 0.3s, box-shadow 0.4s;  
   font-size: 1em; 
-  overflow: hidden; 
+  overflow: hidden;  
+  border-radius: 20px; 
+  box-shadow: 0px 2px 30px 0px rgba(0,0,0,0.1);
 
-  &-text {
+  &-text-outer {
+    padding: 1em;
+  }
+
+  &-date { 
+    transition: opacity $cardTransition; 
     display: flex; 
-    justify-content: space-between; 
-    margin: 0.5em 0 0.5em 0.2em;
+    justify-content: space-between;  
+    align-items: center; 
+    color: #606060; 
+
+    img { 
+      width: 1.3em;  
+      opacity: 0; 
+      transition: opacity 0.3s;
+    }
   }
 
   &:hover {
     transform: scale(1.03);  
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.3);
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.1);
   }  
 
-  &:hover .pen-img {
-    transform: scale(1.05); 
-  }
-  
   &:hover .external-link{
     opacity: 1; 
-  }
-}  
-
-.date {  
-  transition: opacity $cardTransition; 
-  display: flex; 
-  justify-content: space-between;  
-  align-items: center; 
-  color: #606060; 
-
-  img { 
-    width: 1.3em;  
-    opacity: 0; 
-    transition: opacity 0.3s;
   } 
-} 
+}  
 
 .date-text {
   font-size: .7em;
