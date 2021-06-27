0343c1d8e5e7e339ee84b091c66a1ad43f3d7046
Caleb Gibbs <calebgibbs@me.com>
2021-06-27 23:42:46 +1200

    cecil the cow painting

--- src/views/Cecil.vue
+++ src/views/Cecil.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h1 class="gradient-title">Cecil the cow painting</h1> 
+    <h1 class="gradient-title">RIP Cecil the cow painting</h1> 
     <div class="image">
-      <img src="../assets/images/other/cecil.jpg" alt="">
+      <img src="../assets/images/other/cecil.jpg">
     </div>
   </div>
 </template>
@@ -22,12 +22,13 @@ h1 {
 } 
 img { 
   text-align: center; 
-  margin: 0 auto;
+  margin: 0 auto;  
+  height: 100vh;
 } 
 
 .image { 
   margin: 0 auto; 
   text-align: center; 
-  padding-top: 2em;
+  padding-top: 2em; 
 }
 </style>
